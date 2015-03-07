34cfa22f64f278035018514570b397a682911a05
Udbhav Sharma <udbhav.sharma15@gmail.com>
2015-03-07 13:31:40 +0530

    VquadTree init

---
+++ VquadTree/src/structure/Node.java
@@ -0,0 +1,16 @@
+package structure;
+
+import java.util.ArrayList;
+
+public class Node {
+	public Rectangle I;
+	public ArrayList<Node> children = new ArrayList<Node>();
+	public boolean isLeaf = true;
+	
+	public Node(){
+	}
+	
+	public Node(boolean isLeaf){
+		this.isLeaf=false;
+	}
+}
---
+++ VquadTree/src/structure/Tree.java
@@ -0,0 +1,14 @@
+package structure;
+
+public class Tree {
+	
+	public Node root;
+	
+	public Tree(){
+		root=new Node();
+	}
+	
+	public void insert(){
+		
+	}
+}
